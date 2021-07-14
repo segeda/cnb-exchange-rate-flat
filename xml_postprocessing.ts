7302d8e5d51a0dae1622eaf9a7b56f9c8dc55f28
Petr Severa <segedacz@gmail.com>
2021-07-14 18:31:30 +0200

    add XML postprocessing

---
+++ xml_postprocessing.ts
@@ -0,0 +1,33 @@
+import { parse } from "https://deno.land/x/xml@v1.0.2/mod.ts"
+import { readTXT, readJSON, writeJSON } from "https://deno.land/x/flat@0.0.11/mod.ts"
+
+const denni_kurz_filename = Deno.args[0]
+const history_filename = 'history.json'
+
+const denni_kurz = await readTXT(denni_kurz_filename)
+const history = await readJSON(history_filename)
+
+const document: any = parse(denni_kurz)
+
+const datum = document.kurzy['@datum']
+
+const radky = document
+  .kurzy
+  .tabulka
+  .radek
+  .map((radek: any) => ({
+    datum,
+    kod: radek['@kod'],
+    mena: radek['@mena'],
+    mnozstvi: radek['@mnozstvi'],
+    kurz: radek['@kurz'],
+    zeme: radek['@zeme'],
+  }))
+
+for (const radek of radky) {
+  if (!history.find((old: any) => old.datum === radek.datum && old.kod === radek.kod)) {
+    history.push(radek)
+  }
+}
+
+await writeJSON(history_filename, history)
